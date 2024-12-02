92434fd0b0c85fc52fd0c7c7f8a70727daf9b08f
hachimitsu-yogurt <satoiro1112@gmail.com>
2024-12-02 14:57:39 +0900

    Add files via upload

--- kadai_005/bmi.js
+++ kadai_005/bmi.js
@@ -1,11 +1,16 @@
 //変数の宣言
 let bodyWeight;
 let height;
-let bmi;
 
 //値の代入
 bodyWeight = '68';
-height = '1.7'
+height = '1.7';
+
+//BMIの変数宣言
+let bmi;
+
+//BMIの値代入(bmi=bodyWeight / (height * height))
+bmi ='23.529411764705884';
 
 //BMI計算
-console.log(68 / (1.7 * 1.7));
+console.log(bmi);
